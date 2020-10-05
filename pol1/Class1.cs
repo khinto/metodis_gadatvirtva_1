bf6a7e5cc2573426669bed55e6886dddb31ac7f6
khinto <bachana.khintibidze.1@btu.edu.ge>
2020-10-04 23:20:27 -0700

    1

---
+++ pol1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pol1
+{
+    class Class1
+    {
+        public int metodi(int simagle, int pudze)
+        {
+            return simagle * pudze / 2;
+        }
+
+        public int metodi(int gverdi1, int gverdi2, int gverdi3)
+        {
+            return gverdi1 + gverdi2 + gverdi3;
+        }
+
+
+    }
+}
---
+++ pol1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pol1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 obj_1 = new Class1();
+            int simagle, pudze, gverdi1, gverdi2, gverdi3,mart_partobi,perimetri;
+            gverdi1 = int.Parse(textBox1.Text);
+            gverdi2 = int.Parse(textBox2.Text);
+            gverdi3 = int.Parse(textBox3.Text);
+            simagle = int.Parse(textBox4.Text);
+            pudze = int.Parse(textBox5.Text);
+
+
+            mart_partobi = obj_1.metodi(pudze, simagle);
+            perimetri = obj_1.metodi(gverdi1, gverdi2, gverdi3);
+
+            label1.Text = "მართკუთხა სამკუთხედის ფართობია " + mart_partobi.ToString();
+            label2.Text = "სამკუთხედის პერიმეტრია " + perimetri.ToString();
+
+
+
+
+
+        }
+
+        
+    }
+}
